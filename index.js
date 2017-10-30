9a698cdc5dae9c696f0ef16e3a8fc536b6ff03a3
Priswall <31701242+Priswall@users.noreply.github.com>
2017-10-30 15:01:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var canvas = document.getElementById("canvas");
 c = canvas.getContext("2d");
 
+var blocks = [];
+
 function resizeWindow(){
   if(window.innerHeight > window.innerWidth){
     canvas.width = window.innerWidth;
@@ -10,5 +12,39 @@ function resizeWindow(){
     canvas.height = window.innerHeight;
   }
 };
+
+
+function Sprite(x, y, w, h){
+
+  this.x = x;
+  this.y = y;
+  this.w = w;
+  this.h = h;
+  
+  this.draw = function(x, y){
+    c.drawImage(this.y, this.y, this.w, this.h, x, y, this.w, this.h);
+  };
+  
+};
+
+
+var img = new Image();
+img.onload = initSprites(this);
+img.src = "res/sprites.png";
+
+function initSprites(img){
+  
+  for(var i = 0; i < sprites.width/10; i++){
+  
+    blocks[i] = new Sprite(i * 10, i%4, 10, 10);
+    
+  };
+  
+}
+
+var script = document.createElement('script');
+script.src = "js/game.js";
+script.async = true;
+document.body.appendChild(script);
 
 window.addEventListener("resize", resizeWindow);
