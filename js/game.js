139ee1ee74c8d70d957ff0585354f882ddb17170
Priswall <31701242+Priswall@users.noreply.github.com>
2017-10-31 10:16:22 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,7 +1,7 @@
 function loop(){
   c.fillStyle = "white";
   c.fillRect(0, 0, canvas.width, canvas.height);
-  c.drawText("Working!", 50, 50);
+  c.strokeText("Working!", 50, 50);
   window.requestAnimationFrame(loop);
 }
 
