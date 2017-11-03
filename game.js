5f1e17cb8b271d97cf347ff45cd3cc6d89e1b24f
Priswall <31701242+Priswall@users.noreply.github.com>
2017-11-03 13:37:44 -0400

    Update game.js

--- game.js
+++ game.js
@@ -5,4 +5,74 @@ function loop(){
   window.requestAnimationFrame(loop);
 }
 
+function Block(x, y, c){
+  
+  this.x = x;
+  this.y = y;
+  this.c = c;
+  this.show = function(){
+    
+    blocks[0].draw(this.x, this.y, canvas.width/5, canvas.width/5);
+    
+  };
+  
+};
+
+function Player(x, y){
+  
+  this.x = x;
+  this.y = y;
+  this.vel = 0;
+  this.acc = 0.2;
+  this.maxSpeed = 3;
+  this.show = function(){
+    
+    blocks[10].draw(this.x, this.y, canvas.width/5, canvas.width/5);
+    
+  };
+  
+};
+
+function keypress(e){
+
+  if(e.keyCode === 65 || e.keyCode === 37){
+  
+    player.vel += player.acc;
+    if(player.vel > player.maxSpeed){
+      player.vel = player.maxSpeed;
+    }
+    player.x += player.vel;
+    
+  }
+  if(e.keyCode === 68 || e.keyCode === 39){
+  
+    player.vel -= player.acc;
+    if(player.vel < -player.maxSpeed){
+      player.vel = -player.maxSpeed;
+    }
+    player.x -= player.vel;
+    
+  }
+  
+};
+
+function keyrelease(e){
+
+  if(e.keyCode === 65 || e.keyCode === 37 && ){
+  
+    player.vel -= player.acc;
+    player.x -= player.vel;
+    
+  }
+  if(e.keyCode === 68 || e.keyCode === 39){
+  
+    player.vel += player.acc;
+    player.x += player.vel;
+    
+  }
+  
+};
+
 window.requestAnimationFrame(loop);
+window.addEventListener("keydown", keypress);
+window.addEventListener("keyup", keyrelease);
